1745d5daa803ad2ae5697872c2d08df3262bf08d
Justin Firth <jfirth@geekhive.com>
2019-02-14 10:02:03 -0500

    Remove deprecated commands

--- src/gh.ts
+++ src/gh.ts
@@ -4,10 +4,5 @@ import program from 'commander';
 
 program
   .version('1.0.0')
-  .command(
-    'lint [type]',
-    "add linter support for a file type ('js', 'ts', 'scss')"
-  )
-  .command('format', 'add formatting support')
   .command('standards', 'add formatting support')
   .parse(process.argv);
